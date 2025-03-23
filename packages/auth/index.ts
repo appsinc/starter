7b7fb46140fcb7b46d5aca5d8570f088027f7e23
Puru D <puru@dahal.me>
2025-03-23 04:11:01 -0500

    feat: working with better-auth

---
+++ packages/auth/index.ts
@@ -0,0 +1,69 @@
+import * as schema from "@starter/db/schema";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { redirect } from "react-router";
+import db from "@starter/db";
+
+const config = () => {
+	return {
+		basePath: "/api/auth",
+		database: drizzleAdapter(db, {
+			provider: "pg",
+			schema: {
+				user: schema.users,
+				session: schema.sessions,
+				account: schema.accounts,
+				verification: schema.verifications,
+			},
+		}),
+		emailAndPassword: {
+			enabled: false,
+		},
+		account: {
+			accountLinking: {
+				enabled: true,
+			},
+		},
+		session: {
+			cookieCache: {
+				enabled: false,
+			},
+			cookie: {
+				name: "better_auth_session",
+				secure: process.env.NODE_ENV === "production",
+				sameSite: "lax",
+				path: "/",
+			},
+		},
+	} as const;
+};
+
+export const auth = () => {
+	return betterAuth(config());
+};
+
+type getServerSideSessionProps = {
+	request: Request;
+	api?: boolean;
+};
+
+export const getServerSideSession = async ({
+	request,
+	api = false,
+}: getServerSideSessionProps) => {
+	const headers = new Headers(request.headers);
+	const session = await auth().api.getSession({
+		headers,
+	});
+
+	if (!session) {
+		if (api) {
+			throw new Error("Unauthorized");
+		}
+
+		// For regular requests, redirect to auth page
+		throw redirect("/auth");
+	}
+
+	return session;
+};
