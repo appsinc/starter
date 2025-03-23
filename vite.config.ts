0027ecce0234bbf3e6fef5fd646a25f979a839e4
Puru D <puru@dahal.me>
2025-03-23 01:16:31 -0500

    feat: add hono for APIs

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,30 @@
+import adapter from "@hono/vite-dev-server/cloudflare";
 import { reactRouter } from '@react-router/dev/vite'
 import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { getLoadContext } from './load-context'
+import serverAdapter from "hono-react-router-adapter/vite";
 
 export default defineConfig({
-  plugins: [
-    cloudflareDevProxy({ getLoadContext }),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
-  ssr: {
-    resolve: {
-      conditions: ['workerd', 'worker', 'browser'],
-    },
-  },
-  resolve: {
-    mainFields: ['browser', 'module', 'main'],
-  },
-  build: {
-    minify: true,
-  },
-})
+	plugins: [
+		cloudflareDevProxy({ getLoadContext }),
+		reactRouter(),
+		tsconfigPaths(),
+		serverAdapter({
+			adapter,
+			entry: "api/index.ts",
+		}),
+	],
+	ssr: {
+		resolve: {
+			conditions: ["workerd", "worker", "browser"],
+		},
+	},
+	resolve: {
+		mainFields: ["browser", "module", "main"],
+	},
+	build: {
+		minify: true,
+	},
+});
